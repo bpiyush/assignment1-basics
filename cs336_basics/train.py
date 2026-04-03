79024f432fde4574677ea683050ebef17cd1856e
bpiyush <piyushnbagad11@gmail.com>
2026-04-03 22:40:03 +0100

    Adds AdamW optimizer; TODO check if we need separate groups for 2D matrices vs non 2D matrices

--- cs336_basics/train.py
+++ cs336_basics/train.py
@@ -3,21 +3,9 @@ import torch.nn as nn
 import numpy as np
 import einops
 import matplotlib.pyplot as plt
-
-
-# def cross_entropy_(logits, targets):
-#     """
-#     Computes cross-entropy loss for language modelling.
-    
-#     Args:
-#         logits (torch.Tensor): [B, T, V]
-#         targets (torch.Tensor): [B, T]
-#     """
-#     B, T, V = logits.shape
-#     logits_max = logits.max(dim=-1, keepdim=False).values # [B, T]
-#     logits_hat = logits[torch.arange(B)[:, None], torch.arange(T)[None, :], targets] # [B, T]
-#     loss = ((logits_max - logits_hat) + torch.log(torch.exp(logits - logits_max[:, :, None]).sum(dim=-1))).mean()
-#     return loss
+from collections.abc import Callable, Iterable
+from typing import Optional
+import math
 
 
 def cross_entropy(logits, targets):
@@ -48,6 +36,43 @@ def perplexity(logits, targets):
     logits_hat = logits[torch.arange(B)[:, None], torch.arange(T)[None, :], targets] # [B, T]
     x = ((logits_max - logits_hat) + torch.log(torch.exp(logits - logits_max[:, :, None]).sum(dim=-1))) # [B, T]
     return torch.exp(x.mean(dim=-1)).mean()
+
+
+class AdamW(torch.optim.Optimizer):
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.95), eps=1e-8, weight_decay=0.01):
+        defaults = {"lr": lr, "betas": betas, "eps": eps, "weight_decay": weight_decay}
+        super().__init__(params, defaults)
+    
+    def step(self, closure=None):
+        loss = None if closure is None else closure()
+        
+        for group in self.param_groups:
+            lr = group["lr"] # Get the learning rate.
+            b1, b2 = group['betas']
+            eps = group['eps']
+            weight_decay = group['weight_decay']
+            
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                else:
+                    state = self.state[p] # Get state associated with p.
+                    t = state.get("t", 1) # Get iteration number from the state, or initial value.
+                    g = p.grad.data
+                    m = state.get("m", torch.zeros_like(g))
+                    v = state.get("v", torch.zeros_like(g))
+                    
+                    m = b1 * m + (1 - b1) * g
+                    v = b2 * v + (1 - b2) * (g**2)
+                    lr_t = lr * np.sqrt(1 - b2**t) / (1 - b1**t)
+                    p.data -= lr_t * (m / (torch.sqrt(v) + eps))
+                    p.data *= (1 - lr * weight_decay)
+                    
+                    state["t"] = t + 1 # Increment iteration number.
+                    state["m"] = m
+                    state["v"] = v
+
+        return loss
 
 
 if __name__ == "__main__":
